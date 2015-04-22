b2d0799eedad28549e0d0b21feeaca77c8d57509
keisuke07 <bake_uver.like@me.com>
2015-04-22 06:13:02 +0000

    add practice

---
+++ practice.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //これは、コメントです
+    #これもコメントです
+    /*これもコメントで、
+    複数行書き込み可能
+    */
+    echo 'Test';
+    print 'Test';
+    echo "Price", (300 * 1.08), "yen";
+    print('Test');
+    ?>
+</body>
+</html>
